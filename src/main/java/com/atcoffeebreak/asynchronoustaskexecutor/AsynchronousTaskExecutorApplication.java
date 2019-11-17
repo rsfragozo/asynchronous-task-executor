20478a649a7a701b69107f7ce68e3fcc3b210edf
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2019-11-17 14:04:57 +0100

    updated application starter

--- src/main/java/com/atcoffeebreak/asynchronoustaskexecutor/AsynchronousTaskExecutorApplication.java
+++ src/main/java/com/atcoffeebreak/asynchronoustaskexecutor/AsynchronousTaskExecutorApplication.java
@@ -2,8 +2,12 @@ package com.atcoffeebreak.asynchronoustaskexecutor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableRedisRepositories
+@EnableWebMvc
 public class AsynchronousTaskExecutorApplication {
 
     public static void main(String[] args) {
