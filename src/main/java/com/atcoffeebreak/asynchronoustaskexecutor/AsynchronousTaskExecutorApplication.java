3c30428424f938ca98b0d8becb654c3007297380
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2019-11-17 13:42:16 +0100

    source code directory structure created

---
+++ src/main/java/com/atcoffeebreak/asynchronoustaskexecutor/AsynchronousTaskExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.atcoffeebreak.asynchronoustaskexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchronousTaskExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsynchronousTaskExecutorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atcoffeebreak/asynchronoustaskexecutor/AsynchronousTaskExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atcoffeebreak.asynchronoustaskexecutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynchronousTaskExecutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
