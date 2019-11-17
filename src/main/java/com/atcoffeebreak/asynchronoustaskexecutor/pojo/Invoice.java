a264a588fd504bd8ab310e5ac0931c4965f7b69f
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2019-11-17 13:50:25 +0100

    added pojos

---
+++ src/main/java/com/atcoffeebreak/asynchronoustaskexecutor/pojo/Invoice.java
@@ -0,0 +1,13 @@
+package com.atcoffeebreak.asynchronoustaskexecutor.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Invoice {
+    private String id;
+    private String type;
+}
